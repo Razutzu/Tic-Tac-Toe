e25756c66bc21a9503155cf15e1a9cad3f36e572
Razutzu <81825697+Razutzu@users.noreply.github.com>
2024-03-02 19:46:13 +0200

    first commit

---
+++ src/client.js
@@ -0,0 +1,53 @@
+import { Client, Colors, GatewayIntentBits } from "discord.js";
+
+import chalk from "chalk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+class ExtendedClient extends Client {
+	constructor(options) {
+		super(options);
+		this.clr = "#d40000";
+		this.games = new Map();
+		this.embeds = {};
+	}
+	rawData(name) {
+		return data[name];
+	}
+	emoji(name) {
+		return this.emojis.cache.get(data.logs[name]).toString();
+	}
+	emojiData(name) {
+		return this.emojis.cache.get(data.logs[name]);
+	}
+	log(color, tag, msg) {
+		const time = new Date();
+		return console.log(`${chalk.blue(`[ ${time.toLocaleTimeString()} ]`)} ${chalk[color](`[${tag}]`)} ${msg}`);
+	}
+	err(err) {
+		return this.log("red", "  ERROR  ", err.stack || err);
+	}
+	warn(warn) {
+		return this.log("red", " WARNING ", warn);
+	}
+	success(msg) {
+		return this.log("green", " SUCCESS ", msg);
+	}
+	info(info) {
+		return this.log("blue", "  INFO.  ", info);
+	}
+}
+
+const client = new ExtendedClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildEmojisAndStickers] });
+
+client.once("ready", () => client.success(`${client.user.username} is ready`));
+
+process.on("uncaughtException", (err) => client.err(err));
+
+export default client;
+
+// import { events } from "./Handlers/events.js";
+// events();
+
+client.login(process.env.TOKEN);
